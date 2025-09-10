82c1c8424165834e5102b21edeb47ec4b3c2222f
Sharany25 <osoriodiego877@gmail.com>
2025-09-09 23:04:20 -0600

    P3 Produccion

--- src/web-push/push.module.ts
+++ src/web-push/push.module.ts
@@ -3,8 +3,13 @@ import { PushController } from './push.controller';
 import { PushService } from './push.service';
 import { NotificationTaskService } from './notificaciones.service';
 import { SubscriptionService } from './subscription.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscription, SubscriptionSchema } from './schemas/subscription.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Subscription.name, schema: SubscriptionSchema }]),
+  ],
   controllers: [PushController],
   providers: [PushService, NotificationTaskService, SubscriptionService],
 })
